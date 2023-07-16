55cde0ceeace7889a7fcd9bb4ba163153287ea77
Porter-X <waynehere@qq.com>
2023-07-16 21:31:00 +0800

    pointer and cnt

--- String/eightstring.cpp
+++ String/eightstring.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 #define N 100
-
+/* 
 int main(int argc, char const *argv[])
 {
     char arr1[N] = {0};
@@ -22,16 +22,51 @@ int main(int argc, char const *argv[])
     {
         for (int i = 0; i < 8; i++)
             putchar(p[i]);
-        len1 -= 8;
-        p = p + 8;
+        len1 -= 8;//³¤¶ÈÆ«ÒÆ
+        p = p + 8;//Ö¸ÕëÆ«ÒÆ
         puts("");
     }
 
     while ((len1 = strlen(p) )< 8)
     {
-        p[len1] = '0';
+        p[len1] = '0';//²¹Áã
     }
    // arr1[len1 + 1] = '\0';
    puts(p);
+    return 0;
+} */
+
+int main(int argc, char const *argv[])
+{
+    char arr1[N];
+    char *p = arr1;
+    int cnt = 0;
+    gets(arr1);
+
+    while (*p != '\0')
+    {
+        putchar(*p++);
+        if (++cnt == 8)
+        {
+            cnt = 0;
+            puts("");
+        }
+
+    }
+
+/*     while (*p == '\0' && cnt != 0)
+    {
+        putchar('0');
+        cnt++;
+        if (cnt == 8)
+            break;
+    } */
+    if (*p == '\0' && cnt != 0 )
+    {
+        while (cnt++ != 8)
+            putchar('0');
+    }
+
+
     return 0;
 }
