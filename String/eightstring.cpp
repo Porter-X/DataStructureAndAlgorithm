2b6af0b7e7ed7f4b9778dbf0d8004c782ac2c2e8
Porter-X <waynehere@qq.com>
2023-07-16 21:17:44 +0800

    using pointer to char

--- String/eightstring.cpp
+++ String/eightstring.cpp
@@ -3,32 +3,35 @@
 
 using namespace std;
 
-#define N 10
+#define N 100
 
 int main(int argc, char const *argv[])
 {
     char arr1[N] = {0};
     char arr2[N] = {0};
+    char * p;
     int len1, len2;
     gets(arr1);
-    gets(arr2);
+  //  gets(arr2);
+
 
     len1 = strlen(arr1);
-    len2 = strlen(arr2);
-    int shift = len1;
+    p = arr1;
+ //   putchar(*p++);
     while (len1 > 8)
     {
         for (int i = 0; i < 8; i++)
-            putchar(arr1[i]);
+            putchar(p[i]);
         len1 -= 8;
+        p = p + 8;
+        puts("");
     }
 
-    while ((len1 = strlen(arr1) )< 8)
+    while ((len1 = strlen(p) )< 8)
     {
-        arr1[len1] = '0';
-
+        p[len1] = '0';
     }
    // arr1[len1 + 1] = '\0';
-    puts(arr1);
+   puts(p);
     return 0;
 }
