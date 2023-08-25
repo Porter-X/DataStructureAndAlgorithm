9fccdba02d9101772e2d8bd8c3c5f2d24b15f248
Porter-X <waynehere@qq.com>
2023-08-25 22:45:35 +0800

    daily practice

--- Practice/atoi.c
+++ Practice/atoi.c
@@ -42,6 +42,47 @@ int custom_atoi(const char* str) {
     return n*sign;
 } 
 
+
+int myAtoi(char * s){
+    char * p = s;
+    int pOrn = 1;//正
+    int num = 0;
+    while (*p == ' ')
+    {
+        p++;
+    }
+    //putchar(*p);
+    if (*p == '-' || *p == '+')
+    {
+        pOrn = (*p == '-') ? -1 : 1;//简洁的三目运算
+        p++;
+    }
+
+    if (*p < '0' || *p > '9')
+    {
+        return 0;//不满足要求直接返回
+    }
+    while (*p >= '0' && *p <= '9')
+    {
+        if ( num > INT_MAX / 10 ||  (num == INT_MAX / 10 && (*p - '0') > INT_MAX % 10)) //当前num大于最大值的十分之一直接返回，
+                                                                                 
+        { //或者num == 最大值的十分之一并且加上下一个字符会越界
+            if (pOrn == -1)
+                return INT_MIN;
+            else
+                return INT_MAX;
+        }   
+        num = num * 10 + (*p - '0');
+
+        //printf("num :%d\n", num);
+        p++;
+    }
+    printf("max :%d\n", INT_MAX);
+    printf("min :%d\n",INT_MIN);
+    printf("num :%d\n", INT_MAX % 10);
+    return num * pOrn;
+}
+
 int main() {
     char str[] = "12a45";
     int num = custom_atoi(str);
