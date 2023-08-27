eeadc451cb0038e7f704fd7c54c8484fa46afff9
Porter-X <waynehere@qq.com>
2023-08-27 21:21:18 +0800

    merge linklist

--- Practice/mergelinklist.c
+++ Practice/mergelinklist.c
@@ -33,3 +33,50 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
 
     return q;
 }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+
+    struct ListNode dummy;
+    struct ListNode * tail = &dummy;// struct ListNode * tail = (struct ListNode *)malloc(sizeof (struct ListNode));
+    struct ListNode * head = tail;//保存head指针位置
+    
+    if (list1 == NULL && list2 == NULL)
+        return NULL;
+    while (list1 != NULL && list2 != NULL)// 为空时退出循环
+    {
+        if (list1->val < list2->val)
+        {
+            tail->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+        printf("list val %d",tail->val);
+    }
+
+    while (list1 != NULL)
+    {
+        tail->next = list1;
+        list1 = list1->next;
+        tail = tail->next;
+    }
+    while (list2 != NULL)
+    {
+        tail->next = list2;
+        list2 = list2->next;
+        tail = tail->next;
+    }
+
+    return head->next;
+}
